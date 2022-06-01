ef55286e801db8ad73425d651250c606b260cb34
pekingduckk <86128980+pekingduckk@users.noreply.github.com>
2022-05-31 20:10:36 -0700

    Create Tile.js

---
+++ Tile.js
@@ -0,0 +1,241 @@
+export class Tile1 {
+    #tileElement
+    #x
+    #y
+    #value
+ 
+ 
+    constructor(tileContainer, value = 1) {
+        this.#tileElement = document.createElement("div")
+        this.#tileElement.classList.add("tile")
+        tileContainer.append(this.#tileElement)
+        this.value = value
+    }
+ 
+    set value(v) {
+        this.#value = v
+        this.#tileElement.textContent = v
+    }
+ 
+    set x(value) {
+        this.#x = value
+        this.#tileElement.style.setProperty("--x", value)
+    }
+ 
+    set y(value) {
+        this.#y = value
+        this.#tileElement.style.setProperty("--y", value)
+    }
+ 
+}
+ 
+export class Tile2 {
+    #tileElement
+    #x
+    #y
+    #value
+ 
+ 
+    constructor(tileContainer, value = 2) {
+        this.#tileElement = document.createElement("div")
+        this.#tileElement.classList.add("tile")
+        tileContainer.append(this.#tileElement)
+        this.value = value
+    }
+ 
+    set value(v) {
+        this.#value = v
+        this.#tileElement.textContent = v
+    }
+ 
+    set x(value) {
+        this.#x = value
+        this.#tileElement.style.setProperty("--x", value)
+    }
+ 
+    set y(value) {
+        this.#y = value
+        this.#tileElement.style.setProperty("--y", value)
+    }
+ 
+}
+export class Tile3 {
+    #tileElement
+    #x
+    #y
+    #value
+ 
+ 
+    constructor(tileContainer, value = 3) {
+        this.#tileElement = document.createElement("div")
+        this.#tileElement.classList.add("tile")
+        tileContainer.append(this.#tileElement)
+        this.value = value
+    }
+ 
+    set value(v) {
+        this.#value = v
+        this.#tileElement.textContent = v
+    }
+ 
+    set x(value) {
+        this.#x = value
+        this.#tileElement.style.setProperty("--x", value)
+    }
+ 
+    set y(value) {
+        this.#y = value
+        this.#tileElement.style.setProperty("--y", value)
+    }
+ 
+}
+export class Tile4 {
+    #tileElement
+    #x
+    #y
+    #value
+ 
+ 
+    constructor(tileContainer, value = 4) {
+        this.#tileElement = document.createElement("div")
+        this.#tileElement.classList.add("tile")
+        tileContainer.append(this.#tileElement)
+        this.value = value
+    }
+ 
+    set value(v) {
+        this.#value = v
+        this.#tileElement.textContent = v
+    }
+ 
+    set x(value) {
+        this.#x = value
+        this.#tileElement.style.setProperty("--x", value)
+    }
+ 
+    set y(value) {
+        this.#y = value
+        this.#tileElement.style.setProperty("--y", value)
+    }
+ 
+}
+export class Tile5 {
+    #tileElement
+    #x
+    #y
+    #value
+ 
+ 
+    constructor(tileContainer, value = 5) {
+        this.#tileElement = document.createElement("div")
+        this.#tileElement.classList.add("tile")
+        tileContainer.append(this.#tileElement)
+        this.value = value
+    }
+ 
+    set value(v) {
+        this.#value = v
+        this.#tileElement.textContent = v
+    }
+ 
+    set x(value) {
+        this.#x = value
+        this.#tileElement.style.setProperty("--x", value)
+    }
+ 
+    set y(value) {
+        this.#y = value
+        this.#tileElement.style.setProperty("--y", value)
+    }
+ 
+}
+export class Tile6 {
+    #tileElement
+    #x
+    #y
+    #value
+ 
+ 
+    constructor(tileContainer, value = 6) {
+        this.#tileElement = document.createElement("div")
+        this.#tileElement.classList.add("tile")
+        tileContainer.append(this.#tileElement)
+        this.value = value
+    }
+ 
+    set value(v) {
+        this.#value = v
+        this.#tileElement.textContent = v
+    }
+ 
+    set x(value) {
+        this.#x = value
+        this.#tileElement.style.setProperty("--x", value)
+    }
+ 
+    set y(value) {
+        this.#y = value
+        this.#tileElement.style.setProperty("--y", value)
+    }
+ 
+}
+export class Tile7 {
+    #tileElement
+    #x
+    #y
+    #value
+ 
+ 
+    constructor(tileContainer, value = 7) {
+        this.#tileElement = document.createElement("div")
+        this.#tileElement.classList.add("tile")
+        tileContainer.append(this.#tileElement)
+        this.value = value
+    }
+ 
+    set value(v) {
+        this.#value = v
+        this.#tileElement.textContent = v
+    }
+ 
+    set x(value) {
+        this.#x = value
+        this.#tileElement.style.setProperty("--x", value)
+    }
+ 
+    set y(value) {
+        this.#y = value
+        this.#tileElement.style.setProperty("--y", value)
+    }
+ 
+}
+export class Tile8 {
+    #tileElement
+    #x
+    #y
+    #value
+ 
+ 
+    constructor(tileContainer, value = 8) {
+        this.#tileElement = document.createElement("div")
+        this.#tileElement.classList.add("tile")
+        tileContainer.append(this.#tileElement)
+        this.value = value
+    }
+ 
+    set value(v) {
+        this.#value = v
+        this.#tileElement.textContent = v
+    }
+ 
+    set x(value) {
+        this.#x = value
+        this.#tileElement.style.setProperty("--x", value)
+    }
+ 
+    set y(value) {
+        this.#y = value
+        this.#tileElement.style.setProperty("--y", value)
+    }
+ 
+}
